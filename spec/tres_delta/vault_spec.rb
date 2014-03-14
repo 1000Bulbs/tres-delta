dd870604852d364aaff7219c046e14a6fbf88f0d
Zachary Danger Campbell <zacharydangercampbell@gmail.com>
2014-03-14 10:26:01 -0500

    Just use the config

--- spec/tres_delta/vault_spec.rb
+++ spec/tres_delta/vault_spec.rb
@@ -1,5 +1,12 @@
 require 'spec_helper'
 
 describe TresDelta::Vault do
+  let(:config) { TresDelta::Config.config }
+  let(:wsdl) { config["management_wsdl"] }
+
+  it "uses the WSDL from the global config" do
+    expect(TresDelta::Vault.new.wsdl).to eq(wsdl)
+  end
+
   it "handles storing and retrieving credit card details from ThreeDelta"
 end
