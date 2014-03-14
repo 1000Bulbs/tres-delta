c72f00ce2926bb60380d54dfc992272b9ecc00d2
Zachary Danger Campbell <zacharydangercampbell@gmail.com>
2014-03-14 10:23:14 -0500

    Configure the test values from a file

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,6 +3,17 @@ Bundler.setup
 
 require 'tres_delta'
 
+config_file = File.expand_path(File.join(File.dirname(__FILE__), '../config/three_delta.yml'))
+
+unless File.exists?(config_file)
+  puts "You must have a config/three_delta.yml file. See config/three_delta.sample.yml for proper format"
+  exit 1
+end
+
+require 'yaml'
+
+TresDelta::Config.config = YAML.load_file(config_file)
+
 RSpec.configure do |config|
   # TODO: CONFIGURE SOME RSPEC
 end
--- spec/tres_delta/client_spec.rb
+++ spec/tres_delta/client_spec.rb
@@ -2,21 +2,11 @@ require 'spec_helper'
 
 describe TresDelta::Client do
   let(:wsdl) { "http://example.com" }
-  let(:config) do
-    {
-      "client_code" => "1000Bulbs",
-      "password"    => "secrets",
-      "user_name"   => "some_dude"
-    }
-  end
 
   let(:client) { TresDelta::Client.new(wsdl) }
+  let(:config) { TresDelta::Config.config }
 
   describe "#client_credentials" do
-    before(:each) do
-      TresDelta::Config.config = config
-    end
-
     it "uses the credentials passed into it" do
       client.client_credentials.should == {
         "ClientCode" => config["client_code"],
