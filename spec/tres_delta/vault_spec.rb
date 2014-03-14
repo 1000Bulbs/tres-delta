28d8367dbe6a024edfa44d2575e72fb24ef461eb
Zachary Danger Campbell <zacharydangercampbell@gmail.com>
2014-03-14 10:31:48 -0500

    Cleanup spec

--- spec/tres_delta/vault_spec.rb
+++ spec/tres_delta/vault_spec.rb
@@ -3,10 +3,11 @@ require 'spec_helper'
 describe TresDelta::Vault do
   let(:config) { TresDelta::Config.config }
   let(:wsdl) { config["management_wsdl"] }
+  let(:customer) { TresDelta::Customer.new(name: name) }
+  let(:name) { SecureRandom.hex(4) }
+  let(:vault) { TresDelta::Vault.new }
 
   it "uses the WSDL from the global config" do
-    expect(TresDelta::Vault.new.wsdl).to eq(wsdl)
+    expect(vault.wsdl).to eq(wsdl)
   end
-
-  it "handles storing and retrieving credit card details from ThreeDelta"
 end
