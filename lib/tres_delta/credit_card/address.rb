7154e480859b269f9b440389b848e9987f69b380
Zachary Danger Campbell <zacharydangercampbell@gmail.com>
2014-03-17 08:43:25 -0500

    Add address/zip_code attributes

--- lib/tres_delta/credit_card/address.rb
+++ lib/tres_delta/credit_card/address.rb
@@ -1,7 +1,10 @@
 module TresDelta
   class CreditCard
     class Address
+      attr_reader :address, :zip_code
+
       def initialize(params = {})
+        @address  = params[:address]
         @zip_code = params[:zip_code]
       end
     end
