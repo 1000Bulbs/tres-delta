1f734e0703939ec5df30997a42dc0df0cc9a679c
Zachary Danger Campbell <zacharydangercampbell@gmail.com>
2014-03-17 08:48:39 -0500

    Start fleshing out card verification params

--- lib/tres_delta/gateway.rb
+++ lib/tres_delta/gateway.rb
@@ -1,4 +1,53 @@
 module TresDelta
   class Gateway < Client
+    def initialize
+      @wsdl = Config.config['transaction_wsdl']
+    end
+
+    def card_verification(transaction_key, credit_card)
+      request(:card_verification, card_verification_params(transaction_key, credit_card))
+    end
+
+    def card_verification_params(transaction_key, credit_card)
+      {
+        'clientCredentials'      => client_credentials,
+        'cardVerificationParams' => {
+          'AddOrUpdateCard' => 'false',
+          'CreditCard'      => credit_card_params(credit_card),
+          'TerminalIdentifier'   => terminal_identifier,
+          'TransactionKey'       => transaction_key
+        }
+      }
+    end
+
+    def credit_card_params(credit_card)
+      {
+        'BillingAddress'               => billing_address_params(credit_card.billing_address),
+        'cc:CardAccountNumber'         => credit_card.number,
+        'cc:ExpirationMonth'           => credit_card.expiration_month,
+        'cc:ExpirationYear'            => credit_card.expiration_year,
+        'NameOnCard'                   => credit_card.name,
+        'CardSecurityCode'             => nil,
+        'CardSecurityCodeIndicator'    => 'None'
+      }
+    end
+
+    def billing_address_params(billing_address)
+      {
+        'PostalCode' => billing_address.zip_code
+      }
+    end
+
+    def terminal_identifier
+      {
+        'LocationCode' => config['location_code'],
+        'MerchantCode' => config['merchant_code'],
+        'TerminalCode' => config['terminal_code']
+      }
+    end
+
+    def savon_overrides
+      { namespaces: { 'xmlns:cc' => 'http://schemas.datacontract.org/2004/07/ThreeDelta.Web.Services.ECLinx.Definitions' } }
+    end
   end
 end
--- spec/tres_delta/gateway_spec.rb
+++ spec/tres_delta/gateway_spec.rb
@@ -1,4 +1,74 @@
 require 'spec_helper'
 
 describe TresDelta::Gateway do
+  let(:gateway) { TresDelta::Gateway.new }
+
+  it "has a goddamn wsdl" do
+    expect(gateway.wsdl).to eq(TresDelta::Config.config['transaction_wsdl'])
+  end
+
+  describe "credit card address verification" do
+    context "full credit card information" do
+    end
+
+    context "with a token" do
+    end
+  end
+
+  describe "credit card zip code verification" do
+    let(:transaction_key) { SecureRandom.hex(6) }
+
+    # arbitrary: Cisco, TX
+    let(:zip_code_good) { '76437' }
+
+    # magic numbers, see ThreeDelta docs
+    let(:zip_code_failure) { '20151' }
+    let(:zip_code_unavailable) { '32561' }
+    let(:zip_code_error) { '80201' }
+
+    let(:address_params) { { :zip_code => zip_code } }
+
+    let(:credit_card) do
+      TresDelta::CreditCard.new({
+        number:           '4242424242424242',
+        expiration_month: '03',
+        expiration_year:  Time.now.strftime("%Y").to_i + 3,
+        name:             'Joe Customer',
+        type:             'Visa',
+        nickname:         'Test Visa, Yo.',
+        billing_address:  address_params
+      })
+    end
+
+    let(:customer) { TresDelta::Customer.new(name: 'FOO BAR') }
+
+    let(:response) { gateway.card_verification(transaction_key, credit_card) }
+
+    context "good zip code" do
+      let(:zip_code) { zip_code_good }
+
+      it "passes aip code avs" do
+        expect(response.success?).to be_true
+        p response
+      end
+    end
+
+    context "zip code failure" do
+      let(:zip_code) { zip_code_failure }
+
+      it "fails the zip code avs"
+    end
+
+    context "zip code unavailable" do
+      let(:zip_code) { zip_code_unavailable }
+
+      it "doesn't really fail the avs check"
+    end
+
+    context "zip code avs error" do
+      let(:zip_code) { zip_code_error }
+
+      it "doesn't fail per se"
+    end
+  end
 end
