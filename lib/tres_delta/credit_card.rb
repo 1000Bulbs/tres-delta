b9e034ed283788d1d53e4c48c4db10f2a06f8c74
Zachary Danger Campbell <zacharydangercampbell@gmail.com>
2014-03-24 15:29:36 -0500

    Add finder method to CreditCard

--- lib/tres_delta/credit_card.rb
+++ lib/tres_delta/credit_card.rb
@@ -5,7 +5,7 @@ module TresDelta
     attr_accessor :token
 
     def initialize(params = {})
-      @number           = params[:number]
+      @number           = params[:number] || params[:card_account_number]
       @expiration_month = params[:expiration_month]
       @expiration_year  = params[:expiration_year]
       @token            = params[:token]
@@ -24,8 +24,16 @@ module TresDelta
           credit_card.token = add_card.token
         end
       end
+
+      def find(customer, token, load_number = false)
+        stored_card_details = Vault.new.get_stored_credit_card(customer, token, load_number)
+        raise CreditCardNotFound unless stored_card_details.success?
+
+        CreditCard.new(stored_card_details.credit_card)
+      end
     end
   end
 
   class InvalidCreditCard < Exception; end
+  class CreditCardNotFound < Exception; end
 end
--- spec/tres_delta/credit_card_spec.rb
+++ spec/tres_delta/credit_card_spec.rb
@@ -2,6 +2,7 @@ require 'spec_helper'
 
 describe TresDelta::CreditCard do
   let(:customer) { TresDelta::Customer.create(name: "Test Customer") }
+
   let(:bad_params) do
     {
       number:           '4111111111111111',
@@ -12,6 +13,7 @@ describe TresDelta::CreditCard do
       nickname:         'Test Visa, Yo.'
     }
   end
+
   let(:good_params) do
     {
       number:           '4111111111111111',
@@ -23,13 +25,13 @@ describe TresDelta::CreditCard do
     }
   end
 
+  let(:credit_card) { TresDelta::CreditCard.create(customer, good_params) }
+
   describe "#create" do
     context "successfully saved to vault" do
       it "doesn't raise an exception" do
         expect { TresDelta::CreditCard.create(customer, good_params) }.to_not raise_exception
       end
-
-      let(:credit_card) { TresDelta::CreditCard.create(customer, good_params) }
 
       it "has a token" do
         expect(credit_card.token).to_not be_nil
@@ -39,6 +41,29 @@ describe TresDelta::CreditCard do
     context "fails to save to vault" do
       it "raises an exception" do
         expect { TresDelta::CreditCard.create(customer, bad_params) }.to raise_exception(TresDelta::InvalidCreditCard)
+      end
+    end
+  end
+
+  describe "#find", :wip => true do
+    context "card exists" do
+      let(:token) { credit_card.token }
+      let(:found_card) { TresDelta::CreditCard.find(customer, token, true) }
+
+      it "returns a credit card object" do
+        expect(found_card).to be_a(TresDelta::CreditCard)
+        expect(found_card.number).to eq(credit_card.number)
+        expect(found_card.token).to eq(token)
+        expect(found_card.expiration_month).to eq(credit_card.expiration_month)
+        expect(found_card.expiration_year.to_i).to eq(credit_card.expiration_year)
+      end
+    end
+
+    context "not found" do
+      let(:token) { SecureRandom.hex(6) }
+
+      it "raises an error" do
+        expect { TresDelta::CreditCard.find(customer, token) }.to raise_exception(TresDelta::CreditCardNotFound)
       end
     end
   end
